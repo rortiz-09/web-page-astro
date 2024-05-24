92b7b8d4475eb65b296fd9a81de7e401092d9de0
rortiz-09 <ronny.ortiz.54@hotmail.com>
2024-05-23 19:23:24 -0500

    Update Pricing & 404

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,23 @@ import animations from 'tailwindcss-animated'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'red-orange': {
+					'50': '#fff2f1',
+					'100': '#ffe1e0',
+					'200': '#ffc9c7',
+					'300': '#ffa4a0',
+					'400': '#ff6f69',
+					'500': '#f9423a',
+					'600': '#e7241b',
+					'700': '#c21a13',
+					'800': '#a01a14',
+					'900': '#851c17',
+					'950': '#480a07',
+				},
+			},
+		},
 	},
 	plugins: [animations],
 }
